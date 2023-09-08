a7afd311e98abff347531003e25017c526d32337
oz <32149009+shallowbug@users.noreply.github.com>
2023-09-08 16:32:20 -0700

    data output

--- page-myshop.php
+++ page-myshop.php
@@ -7,12 +7,58 @@
     the_post_thumbnail('medium');
 
     the_content();
+    the_field('blurb');
+
+    if( have_rows('header') ):
+      while( have_rows('header') ): the_row();
+        the_sub_field('blurb');
+        the_sub_field('stats');
+        the_sub_field('mood');
+        endwhile;
+    endif;
+
+    if( have_rows('interests') ):
+      while( have_rows('interests') ): the_row();
+        the_sub_field('general');
+        the_sub_field('music');
+        the_sub_field('movies');
+        the_sub_field('tv');
+        the_sub_field('books');
+        the_sub_field('heroes');
+
+        endwhile;
+    endif;
+
+    if( get_field('details_status') ):
+        echo 'DEET:'; the_field('details_status');
+    endif;
+
+    if( get_field('details_here') ):
+        echo 'HERE:'; the_field('details_here');
+    endif;
+    the_field('details_hometown');
+    the_field('details_ethnicity');
+    the_field('details_zodiac');
+    the_field('details_smokedrink');
+    the_field('details_education');
+    the_field('details_occupation');
+
+    $get_posts = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1));
+
+    if ( $get_posts -> have_posts() ) :
+        while ( $get_posts -> have_posts() ) : $get_posts -> the_post();
+            the_permalink();
+            the_title();
+        endwhile;
+    endif;
 
     if ( have_posts() ) :
         while ( have_posts() ) : the_post();
             // Display post content
         endwhile;
     endif;
+
+    do_shortcode('[woocommerce_product_filter_products]');
 
 /* Start the Loop */
     while ( have_posts() ) :
@@ -24,5 +70,6 @@
         }
     endwhile; // End of the loop.
 
-    get_footer(); 
+    get_footer();
+
 ?>
--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -17,38 +17,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-
 get_header( 'shop' );
-
-/**
- * Open Left Panel
- */
-get_template_part('templates/partials/left-panel-open');
-
-$shop_color = get_theme_mod('shop_color', '#FEE100');
-$shop_color_2 = get_theme_mod('shop_color_2', '#485DFF');
-\JPeraTheme\View::render('BgBlocks', [
-    'blocks' => [
-        ['color' => $shop_color, 'rounded_class' => 'rounded-3xl'],
-        ['color' => $shop_color_2, 'rounded_class' => 'rounded-full']
-    ]
-]);
-?>
-
-
-<div class="h-full w-full flex flex-col text-center">
-    <?php
-    \JPeraTheme\View::render('ProductFilters');
-    ?>
-</div>
-
-
-<?php
-/**
- * Open Right Panel
- */
-get_template_part('templates/partials/left-panel-close');
-get_template_part('templates/partials/right-panel-open');
 
 /**
  * Hook: woocommerce_before_main_content.
@@ -58,46 +27,23 @@ get_template_part('templates/partials/right-panel-open');
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
 do_action( 'woocommerce_before_main_content' );
-\JPeraTheme\View::render('MiniCart');
+
 ?>
+<header class="woocommerce-products-header">
+	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+	<?php endif; ?>
 
-<style>
-    @media only screen and (max-width: 768px) {
-        .right-panel {
-            max-height: 50%;
-        }
-    }
-</style>
-
-<div class="marquee-wrapper flex">
-    <div class="marquee3k h-0 md:h-auto overflow-hidden mb-2 mr-24 xl:mr-40" data-speed="0.25">
-        <span class="marquee-text uppercase text-xl 2xl:text-2xl pr-6">
-        <?php echo get_option('marquee', 'this is a marquee test '); ?>
-        </span>
-    </div>
-    <ul class="flex ml-auto text-2xl font-normal">
-        <li class="mr-6">
-            <a href="/">Shop</a>
-        </li>
-        <li class="mr-6">
-            <a href="#" class="nav-info">Info</a>
-        </li>
-        <li class="mr-6">
-            <a href="https://www.instagram.com/j.pera_/" target="_blank">Social</a>
-        </li>
-    </ul>
-    <div class="cartLink w-10 md:w-10 ml-4">
-        <div data-opens="#miniCart">
-            <?php
-            \JPeraTheme\View::render('ShoppingCartLink');
-            ?>
-        </div>
-        <style>
-            .cart-size { filter: invert(1) }
-        </style>
-    </div>
-</div>
-
+	<?php
+	/**
+	 * Hook: woocommerce_archive_description.
+	 *
+	 * @hooked woocommerce_taxonomy_archive_description - 10
+	 * @hooked woocommerce_product_archive_description - 10
+	 */
+	do_action( 'woocommerce_archive_description' );
+	?>
+</header>
 <?php
 if ( woocommerce_product_loop() ) {
 
@@ -108,7 +54,7 @@ if ( woocommerce_product_loop() ) {
 	 * @hooked woocommerce_result_count - 20
 	 * @hooked woocommerce_catalog_ordering - 30
 	 */
-	// do_action( 'woocommerce_before_shop_loop' );
+	do_action( 'woocommerce_before_shop_loop' );
 
 	woocommerce_product_loop_start();
 
@@ -142,15 +88,18 @@ if ( woocommerce_product_loop() ) {
 	do_action( 'woocommerce_no_products_found' );
 }
 
-
-get_template_part('templates/partials/right-panel-close');
-
-
 /**
  * Hook: woocommerce_after_main_content.
  *
  * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
  */
 do_action( 'woocommerce_after_main_content' );
+
+/**
+ * Hook: woocommerce_sidebar.
+ *
+ * @hooked woocommerce_get_sidebar - 10
+ */
+do_action( 'woocommerce_sidebar' );
 
 get_footer( 'shop' );
