c106d3f512348d1c3710b52c8431e114172eac6c
oz <32149009+shallowbug@users.noreply.github.com>
2023-09-14 13:37:09 -0700

    prelim design

--- page-myshop.php
+++ page-myshop.php
@@ -1,19 +1,81 @@
 <?php get_header(); ?>
-<header>
-    <nav>
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Store</a></li>
-            <li><a href="">Blog</a></li>
-            <li><a href="">Photos</a></li>
-            <li><a href="">Instagram</a></li>
-            <li><a href="">Playground</a></li>
-            <li><a href="">Coffee</a></li>
-            <li><a href="">Contact</a></li>
-        </ul>
-    </nav>
-</header>
 
+<?php get_template_part( 'templates/partials/header-myshop' ); ?>
+
+<div class="flex flex-col">
+    <div id="profileHeader" class="flex flex-row">
+        <div id="profilePic" class="flex flex-col">
+            <?php the_title(); ?>
+            <?php the_post_thumbnail('medium'); ?>
+            <span id="mood" class="inline-block"><b>Mood:</b> <?php get_field('header_mood') ? the_field('header_mood') : null ;?></span>
+
+        </div>
+        <div id="profileBlurb" class="flex flex-col">
+            <?php ?>
+            <?php ?>
+            <?php ?>
+        </div>
+    </div>
+
+
+</div>
+<script>
+    // Create a new XMLHttpRequest object
+        var xhr = new XMLHttpRequest();
+
+        // Define the URL to fetch
+        var url = 'https://letterboxd.com/shallowbug/films/diary/by/entry-rating/';
+
+        // Configure the request
+        xhr.open('GET', url, true);
+
+        // Set up a function to handle the response
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                // Parse the HTML content
+                var parser = new DOMParser();
+                var doc = parser.parseFromString(xhr.responseText, 'text/html');
+
+                // Find the "film-table" element
+                var filmTable = doc.querySelector('.film-table');
+
+                // Initialize a counter to keep track of the number of items extracted
+                var count = 0;
+
+                // Iterate through the "headline-3" elements inside "film-table"
+                var headlines = filmTable.querySelectorAll('.headline-3');
+                for (var i = 0; i < headlines.length; i++) {
+                    // Extract the text from each element
+                    var text = headlines[i].textContent;
+
+                    // Display the text on your webpage
+                    var resultDiv = document.getElementById('result');
+                    var paragraph = document.createElement('p');
+                    paragraph.textContent = text;
+                    resultDiv.appendChild(paragraph);
+
+                    // Increment the counter
+                    count++;
+
+                    // Stop after extracting the first 20 items
+                    if (count >= 20) {
+                        break;
+                    }
+                }
+            } else {
+                console.error('Request failed with status:', xhr.status);
+            }
+        };
+
+        // Handle errors
+        xhr.onerror = function () {
+            console.error('Request failed');
+        };
+
+        // Send the request
+        xhr.send();
+</script>
+<div id="result"></div>
 <?php ?>
 <?php ?>
 <?php ?>
@@ -23,9 +85,9 @@
 
 <?php
 
-    the_title();
+    
 
-    the_post_thumbnail('medium');
+    
 
     the_content();
     the_field('blurb');
---
+++ templates/partials/header-myShop.php
@@ -0,0 +1,14 @@
+<header class="flex flex-col justify-end">
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Store</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Photos</a></li>
+            <li><a href="">Instagram</a></li>
+            <li><a href="">Playground</a></li>
+            <li><a href="">Coffee</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+</header>
